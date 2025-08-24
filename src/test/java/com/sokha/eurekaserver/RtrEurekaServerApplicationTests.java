309d03ba7925c6a578f5447a3e0f06ef82bfc5ac
khinsokha <khinsokha@gmail.com>
2025-08-24 18:58:19 +0700

    first commit

---
+++ src/test/java/com/sokha/eurekaserver/RtrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sokha.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
